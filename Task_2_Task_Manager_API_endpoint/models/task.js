112916be1f43226b9b47aadf3668715602413641
haile121 <haileag8@gmail.com>
2025-08-27 03:38:33 +0300

    Add task.js model for Task 2 API project

---
+++ Task_2_Task_Manager_API_endpoint/models/task.js
@@ -0,0 +1,12 @@
+class Task {
+    constructor(id, title, description, dueDate, status) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.status = status;
+    }
+  }
+  
+  export default Task;
+  
